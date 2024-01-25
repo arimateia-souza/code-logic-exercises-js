8b80bd0c9e0a5e588e2a564681c47ee76d4d7353
Ari Souza <email.arisantos@gmail.com>
2024-01-25 13:37:37 -0300

    lista

---
+++ js-curso-5/lista.js
@@ -0,0 +1,15 @@
+// 1 - Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+// 2 - Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+
+// 3 - Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang')
+// 4 - Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomePessoa = ['Ari', 'Ayla', 'Nikolas'];
+console.log(nomePessoa[0]);
+// 5 - Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(nomePessoa[1]);
+// 6 - Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(nomePessoa[2]);
